3b8e6b18d1fb693f98851f218b1b78d337e697c4
ciberseguranca <linonetopacheco@enta.pt>
2023-03-20 08:46:25 -0100

    .

--- cybersecurity/variables.tf
+++ cybersecurity/variables.tf
@@ -3,7 +3,6 @@ variable "public_key" {
   default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtbkznNFOHYzW9Dw0syf16wrDx5PFXG1padPew/WrEt3rBix3CO4xG1RzdUQ3Ky5gnKkCgZ36VRptUKHy2SZ0DAon9DI/L4PUC5bh0GuWpJK3parJoaJ9lO84Rw9xLilFHj2akDSgCZZB9jJFzb6N0BejOFPBjAfKg+3oIzXUG1rUIm72y7rUaFpWpTgzmzxriqNvrGmlRhvaeq1Z8tyVrexh9T0CaQe5E1sQozzyfcuNaxZJMFvIT3FMS+zn4qnnhqcNU0/WGDpr//Tf+2Kc1TPZqogLDyQ5dt1ufPMQf5AMjiv0/YL/fXG5e4yGi+vobggkSD5upb2BwlCyPMGKd"
 }
 
-
 variable "desktop_ami" {
   type = string
   default = "ami-0557a15b87f6559cf"
